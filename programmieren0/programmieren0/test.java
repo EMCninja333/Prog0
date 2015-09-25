99a2e3ae557eb23c416d20c64da834ce0f799351
emcNinja <m1faessl@hsr.ch>
2015-09-25 14:49:53 +0200

    Projekt Initialisierung

---
+++ programmieren0/programmieren0/test.java
@@ -0,0 +1,20 @@
+package programmieren0;
+
+public class test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		testMe();
+	}
+	
+	/**
+	 * Ich mache nichts :P
+	 * @param x
+	 * @param y
+	 * @return void
+	 */
+	public static void testMe(){
+		
+	}
+
+}
